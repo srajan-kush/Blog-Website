6a6ca56ecf357b16cc0af94c23562bfd976895ea
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-02-28 00:04:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require('express');
+const userRoute = require('./routes/user');
+
+app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views"));
+
+
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.use('/user',userRoute);
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
