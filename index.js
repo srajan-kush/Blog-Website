1382bd09ebaf29704f33d4a53ff6b58f6810bb76
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2025-03-02 23:34:19 +0530

    adding Blog model + schema

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const express = require('express');
 const userRoute = require('./routes/user');
+const blogRouter = require("./routes/blog");
 
 const cookieParser = require('cookie-parser');
+
+const Blog = require('./models/blog');
 
 const mongoose = require("mongoose");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
@@ -19,12 +22,17 @@ app.set("views", path.resolve("./views"));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.resolve("./public")));
+
 
 app.use('/user',userRoute);
+app.use('/blog',blogRouter);
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const allBlog = await Blog.find({});
     return res.render("home",{
         user: req.user,
+        blogs: allBlog,
     });
 });
 
